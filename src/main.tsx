c6745dd3065593633f7959e8bc9111ecabf6049c
MiguelC007 <103707813+MiguelC007@users.noreply.github.com>
2026-07-05 14:18:16 -0600

    Load final polish stylesheet

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./app/App";
 import "./styles/variables.css";
 import "./styles/global.css";
+import "./styles/final-polish.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
